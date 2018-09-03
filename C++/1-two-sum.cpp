ae4fa49e49898dce121a5c18e2087634c73b2823
John Kindem <461425614@qq.com>
2018-09-04 00:22:57 +0800

    [add] cpp - 1 Two Sum (Tecent)

---
+++ C++/1-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for (int i = (int) (nums.size() - 1); i > -1; i--) {
+            for (auto j = i - 1; j > -1; j--) {
+                if (nums[i] + nums[j] == target) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
