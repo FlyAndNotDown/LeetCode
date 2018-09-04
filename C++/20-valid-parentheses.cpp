9fb3f7c40730e671beac406d971ea490258249a8
John Kindem <461425614@qq.com>
2018-09-04 19:40:11 +0800

    [add] cpp - 20 Valid Parentheses (Tecent)

---
+++ C++/20-valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string input) {
+        list<char> stack;
+        for (auto c : input) {
+            if (c == '{') stack.push_back(c);
+            if (c == '[') stack.push_back(c);
+            if (c == '(') stack.push_back(c);
+            if (c == '}') { if (!stack.empty() && stack.back() == '{') stack.pop_back(); else return false; }
+            if (c == ']') { if (!stack.empty() && stack.back() == '[') stack.pop_back(); else return false; }
+            if (c == ')') { if (!stack.empty() && stack.back() == '(') stack.pop_back(); else return false; }
+        }
+        return stack.empty();
+    }
+};
