a0e46876b41edb082b75bcc3b7f9ff45edd971a0
John Kindem <461425614@qq.com>
2018-09-06 00:07:18 +0800

    [add] python - 231

---
+++ Python/231-power-of-two.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return False if n < 0 else True if n == 1 else False if n == 0 else self.isPowerOfTwo(n // 2) if n % 2 == 0 else False
