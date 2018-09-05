164056c718644ac3f3d1382e437e8a858d118995
John Kindem <461425614@qq.com>
2018-09-06 00:00:23 +0800

    [add] Python - 23

---
+++ Python/23-merge-k-sorted-lists.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        l = []
+        for list in lists:
+            p = list
+            while p:
+                l.append(p.val)
+                p = p.next
+        l.sort()
+        answer = ListNode(0)
+        p = answer
+        for num in l:
+            p.next = ListNode(num)
+            p = p.next
+        return answer.next
