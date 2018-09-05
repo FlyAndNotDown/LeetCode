58126be1b281ec442ef74c949d91801eb7e0c556
John Kindem <461425614@qq.com>
2018-09-06 00:01:03 +0800

    [add] python - 61

---
+++ Python/61-rotate-list.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head: return None
+        elif not head.next: return head
+
+        length = 1
+        p = head
+        while p.next:
+            length += 1
+            p = p.next
+        p.next = head
+
+        k = k % length
+        for _ in range(length - k):
+            head = head.next
+            p = p.next
+        p.next = None
+        return head
